bacb5c15cd234d997437a184526641e1437cb32b
asahi kojima <ubutara12@gmail.com>
2025-12-24 01:03:31 +0900

    first commit

---
+++ word_practice.py
@@ -0,0 +1,6 @@
+# 英語と日本語を分離
+english_words = [pair[0] for pair in word_pairs]
+japanese_words = [pair[1] for pair in word_pairs]
+
+print("English:", english_words)
+print("Japanese:", japanese_words)
